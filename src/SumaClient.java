0dcdcb07429577b82355a062927eb9c9ba8934bb
USUARIO <USUARIO@PC16>
2018-04-11 21:50:02 -0500

    se soluciono el problema que se presentaba

--- src/SumaClient.java
+++ src/SumaClient.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package clase2;
-
 import java.util.Scanner;
 import sumaApp.*;
 //importar la interfaz idl
